b8d7c483a34986f62a1428db7f7bd93b27b7b882
AloeLavi <story_teller@bk.ru>
2023-01-02 16:07:13 +0300

    added step name

--- src/test/java/tests/browserstack/SelenideSearchTests.java
+++ src/test/java/tests/browserstack/SelenideSearchTests.java
@@ -6,16 +6,23 @@ import org.junit.jupiter.api.Test;
 import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
 import static com.codeborne.selenide.Condition.text;
 import static com.codeborne.selenide.Selenide.*;
+import static io.qameta.allure.Allure.step;
 
 public class SelenideSearchTests extends TestBase {
 
     @Test
     void successSearchTest() {
         back();
-        $(AppiumBy.accessibilityId("Search Wikipedia")).click();
-        $(AppiumBy.id("org.wikipedia.alpha:id/search_src_text")).sendKeys("Twin Peaks");
-        $$(AppiumBy.id("org.wikipedia.alpha:id/page_list_item_title")).shouldHave(sizeGreaterThan(0));
-        $(AppiumBy.id("org.wikipedia.alpha:id/page_list_item_title")).click();
-        $(AppiumBy.className("android.widget.TextView")).shouldHave(text("Twin Peaks"));
+        step("Нажать на 'Search Wikipedia'"), () -> {
+             $(AppiumBy.accessibilityId("Search Wikipedia")).click();
+        });
+        step("Ввести 'Twin Peaks'", () -> {
+             $(AppiumBy.id("org.wikipedia.alpha:id/search_src_text")).sendKeys("Twin Peaks");
+             $$(AppiumBy.id("org.wikipedia.alpha:id/page_list_item_title")).shouldHave(sizeGreaterThan(0));
+        });
+        step("Открыть статью 'Twin Peaks'", () -> {
+            $(AppiumBy.id("org.wikipedia.alpha:id/page_list_item_title")).click();
+            $(AppiumBy.className("android.widget.TextView")).shouldHave(text("Twin Peaks"));
+        });
     }
 }
